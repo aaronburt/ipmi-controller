594e5dcf08586bd6d20670831e86ed8f363ae204
aaronburt <email@aaronburt.co.uk>
2023-07-08 19:02:14 +0100

    Intentionally uploading application.properties, these credentials are just an example and don't work.

--- src/main/java/uk/co/aaronburt/ipmi/controller/UIController.java
+++ src/main/java/uk/co/aaronburt/ipmi/controller/UIController.java
@@ -1,20 +1,35 @@
 package uk.co.aaronburt.ipmi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/")
 public class UIController {
 
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "Index";
     }
 
     @GetMapping("/error")
-    public String catchAll(){
+    public String catchAll() {
         return "Error";
+    }
+
+    @GetMapping("/embed/button/{button}")
+    public String button(@PathVariable String button, Model model) {
+        if (Objects.equals(button, "start")) {
+            model.addAttribute("isStartButton", true);
+        } else {
+            model.addAttribute("isHaltButton", true);
+        }
+
+        return "PowerButton";
     }
 }
